f71d6d7861f4d53a85a22b9e27aa25b779dcc676
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2020-03-16 20:55:59 +0100

    wip: programming cards

---
+++ Privat/Schatzjagd/Cards/Play/Cards.ts
@@ -0,0 +1,49 @@
+namespace SchatzjagdCards {
+  let card: HTMLDivElement;
+  window.addEventListener("load", hndLoad);
+
+  function hndLoad(_event: Event): void {
+    for (let key in data) {
+      let cardData = data[key];
+      card = createCard(cardData);
+    }
+  }
+
+  function createCard(_data: Object): HTMLDivElement {
+    let card: HTMLDivElement = document.createElement("div");
+    let style: CSSStyleDeclaration = card.style;
+
+    if (_data["background"])
+      style.backgroundImage = `url("${_data["background"]}"`;
+
+    let headline: HTMLHeadElement = document.createElement("h1");
+    headline.textContent = _data["head"] || "................";
+    card.appendChild(headline);
+
+    if (_data["image"])
+      card.appendChild(createImage(_data["image"]));
+
+    // if (_data["text"])
+    //   card.appendChild(createText(_data["text"]));
+
+    document.body.appendChild(card)
+    return card;
+  }
+
+  function createImage(_data: Object): HTMLImageElement {
+    let image: HTMLImageElement = document.createElement("img");
+    image.src = _data["url"];
+    image.style.transform = `scale(${_data["scale"]})`;
+    image.style.top = _data["top"] + "mm";
+    image.style.left = _data["left"] + "mm";
+    return image;
+  }
+
+  function createText(_data: Object): HTMLSpanElement {
+    let span: HTMLSpanElement = document.createElement("span");
+    span.textContent = _data["content"];
+    // span.style.top = _data["top"] + "mm";
+    // span.style.left = _data["left"] + "mm";
+    return span;
+  }
+}
---
+++ Privat/Schatzjagd/Cards/Play/Data.ts
@@ -0,0 +1,14 @@
+namespace SchatzjagdCards {
+  export let data = {
+    test: { background: "CardFrontExample.svg" },
+    card0: {
+      head: "Hunne",
+      image: { url: "../Images/Unhold_3_0.png", scale: 0.193, top: 8, left: 9 },
+      text: { content: "Fliehst Du, verlierst Du all dein Gold!", top: 0, left: 0 }
+    },
+    card1: { head: "Räuber" },
+    card2: { head: "Germane" },
+    card3: { head: "Riese" },
+    card4: { head: "Warpig" },
+  };
+}
