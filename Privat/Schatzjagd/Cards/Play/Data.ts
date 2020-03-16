cb7b84924a6e40030fd8d95721d720fa0987e2d7
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2020-03-17 00:28:48 +0100

    Sonderzeichen

--- Privat/Schatzjagd/Cards/Play/Data.ts
+++ Privat/Schatzjagd/Cards/Play/Data.ts
@@ -6,14 +6,14 @@ namespace SchatzjagdCards {
   let horzMargin: number = 10;
   let vertBase: number = 40;
   let vertOffset: number = 12;
-
+// ÷ ½ × ˗ ˖ ₊ 
   export let data = {
     test: { background: "CardFrontExample.svg" },
     card0: {
       head: "Hunne",
       image: { url: "../Images/Unhold_3_0.png", scale: 0.193, top: 8, left: 9 },
       text: { content: "Fliehst Du,<br/>verlierst Du alles Gold!"},
-      markers: [{left: horzMargin, top: vertBase, color: yellow, content: "½"}]
+      markers: [{left: horzMargin, top: vertBase, color: yellow, content: "₊2"}]
     },
     card1: {
       head: "Gasthaus",
