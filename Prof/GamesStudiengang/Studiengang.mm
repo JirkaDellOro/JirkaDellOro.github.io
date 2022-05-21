be04920858ce98454e2283fc7d17f06a0568d64d
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2022-05-21 11:13:13 +0200

    Games Studiengang

---
+++ Prof/GamesStudiengang/Studiengang.mm
@@ -0,0 +1,127 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1653027317845" ID="ID_1110058023" MODIFIED="1653027402328">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Studiengang
+    </p>
+    <p style="text-align: center">
+      immersive Medien
+    </p>
+    <p style="text-align: center">
+      und Games
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1653027464693" FOLDED="true" ID="ID_1775894993" MODIFIED="1653054155857" POSITION="right" TEXT="Kurzbeschreibung des Studiengangs &#xa;Zentrale Inhalte und Ziele, Berufsbilder f&#xfc;r Absolventen?">
+<arrowlink DESTINATION="ID_1775894993" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_845656212" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1775894993" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_845656212" SOURCE="ID_1775894993" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1653027469867" ID="ID_938214120" MODIFIED="1653049363420" TEXT="Konzeption, Gestaltung, Entwicklung und Sozio-&#xd6;konomie immersiver Medien und Computergames. "/>
+<node CREATED="1653027482399" FOLDED="true" ID="ID_520856873" MODIFIED="1653054154884" TEXT="Innerhalb der Gamesbranche">
+<node CREATED="1653027569687" ID="ID_1201190285" LINK="https://www.gamecampus.de/arbeiten/berufsbilder" MODIFIED="1653027701023" TEXT="typische Berufsbilder wie... (siehe  Gamecampus)"/>
+<node CREATED="1653027578163" ID="ID_733613866" MODIFIED="1653049821742" TEXT="Gamedesign"/>
+<node CREATED="1653027592469" ID="ID_1105568527" MODIFIED="1653027594281" TEXT="Technical Artist"/>
+<node CREATED="1653027585790" ID="ID_1688785978" MODIFIED="1653049798760" TEXT="Gameprogramming"/>
+<node CREATED="1653027600696" ID="ID_1452081441" MODIFIED="1653049814690" TEXT="Producing"/>
+<node CREATED="1653029898809" ID="ID_727397067" MODIFIED="1653049817553" TEXT="Sound Design"/>
+<node CREATED="1653032120436" ID="ID_1394933537" MODIFIED="1653032122060" TEXT="..."/>
+</node>
+<node CREATED="1653027717109" FOLDED="true" ID="ID_1427067910" MODIFIED="1653054153315" TEXT="Au&#xdf;erhalb der Gamesbranche">
+<node CREATED="1653027750609" ID="ID_1974596687" MODIFIED="1653027824869" TEXT="Computergames pr&#xe4;gen -&gt; Erwartungshaltung an Produkte"/>
+<node CREATED="1653050100813" ID="ID_125969349" MODIFIED="1653050441599" TEXT="Simulation und Virtualisierung"/>
+<node CREATED="1653050325123" ID="ID_968193139" MODIFIED="1653050424341" TEXT="Immersive Kommunikation, Social Media"/>
+<node CREATED="1653027849651" ID="ID_1853796300" MODIFIED="1653050431919" TEXT="Digital Design"/>
+<node CREATED="1653027827077" ID="ID_57492697" MODIFIED="1653027860010" TEXT="Mediendesign"/>
+<node CREATED="1653027840642" FOLDED="true" ID="ID_1177755673" MODIFIED="1653050081342" TEXT="Medienkunst">
+<node CREATED="1653032091668" ID="ID_929890945" MODIFIED="1653032110584" TEXT="Wir sehen Games als eine k&#xfc;nstlerische Ausdrucksform an, mit der Kreative &#xe4;sthetische Innovationen erproben, transdisziplin&#xe4;re Br&#xfc;cken schlagen und gesellschaftliche Diskurse begleiten (Games-Strategie des Bundes)"/>
+</node>
+<node CREATED="1653027854807" ID="ID_201617039" MODIFIED="1653050426496" TEXT="Medieninformatik"/>
+<node CREATED="1653052182307" ID="ID_1263838059" MODIFIED="1653052187151" TEXT="KI in den Medien"/>
+</node>
+</node>
+<node CREATED="1653027920644" FOLDED="true" HGAP="32" ID="ID_1386468589" MODIFIED="1653052845490" POSITION="right" TEXT="Attraktivit&#xe4;t/Marktpotenzial des Studiengangs&#xa;Erwartete Nachfrage, neue Potenziale von Studieninteressierten,&#xa;Frauenanteil, internationale Wettbewerbsf&#xe4;higkeit?">
+<node CREATED="1653028020326" ID="ID_90086168" MODIFIED="1653028043412" TEXT="Computergames sind heute Leitmedium"/>
+<node CREATED="1653051340128" ID="ID_915091221" MODIFIED="1653051355821" TEXT="eSports sind Publikumsmagneten"/>
+<node CREATED="1653051693919" ID="ID_1876561394" MODIFIED="1653051710359" TEXT="Zielgruppe aufgewachsen mit digitalen Medien"/>
+<node CREATED="1653028209326" FOLDED="true" ID="ID_1420633715" MODIFIED="1653050759088" TEXT="Umsatz der Computerspielbranche seit Langem deutlich h&#xf6;her als Musik und Filmbranche in Kombination">
+<node CREATED="1653031571303" ID="ID_60830353" MODIFIED="1653031600058" TEXT="8,5 Mrd. Euro Umsatz im Jahr 2020 zeigen: Computerspiele sind l&#xe4;ngst ein wichtiger Wirtschaftsfaktor. Etwa 590 Unternehmen entwickeln in Deutschland Computerspiele. Sie schaffen und sichern Arbeitspl&#xe4;tze, bilden aus und tragen zur Wertsch&#xf6;pfung bei. Durch die Bildung regionaler Cluster leisten sie einen wichtigen Beitrag zur lokalen Standortf&#xf6;rderung. (Games-Strategie des Bundes)"/>
+</node>
+<node CREATED="1653028044707" ID="ID_927959650" MODIFIED="1653028204317" TEXT="Entsprechend hoch ist das Interesse, sie mitzugestalten."/>
+<node CREATED="1653028057637" ID="ID_1632000567" MODIFIED="1653050675322" TEXT="Frauen und M&#xe4;nner spielen gleicherma&#xdf;en, aber anders"/>
+<node CREATED="1653028076961" FOLDED="true" ID="ID_1011708312" MODIFIED="1653050756905" TEXT="Branche sehr divers, international, etwa ein Drittel Frauen.">
+<node CREATED="1653031634053" ID="ID_246917073" MODIFIED="1653031657265" TEXT="Games unterst&#xfc;tzen Vielfalt Durch Computerspiele treffen Menschen mit unterschiedlichen Hintergr&#xfc;nden aufeinander, um gemeinsam zu spielen, unabh&#xe4;ngig von Geschlecht und geschlechtlicher Identit&#xe4;t, Nationalit&#xe4;t, ethnischer oder sozialer Herkunft, Bildungsstand, Religion, Behinderung, Alter, Sprache und sexueller Orientierung. Computerspiele sind ein Medium, welches wie wenige andere f&#xfc;r Diversit&#xe4;t steht, Teilhabe erm&#xf6;glicht und Inklusion bewirkt. (Games-Strategie des Bundes)"/>
+</node>
+<node CREATED="1653051007414" ID="ID_1103411059" MODIFIED="1653051042242" TEXT="Neue Formen der Interaktion und Partizipation"/>
+<node CREATED="1653051218061" ID="ID_1620789920" MODIFIED="1653051434327" TEXT="N&#xe4;he zu Innovationen in Unterhaltung, Industrie und Gesellschaft"/>
+<node CREATED="1653028123923" ID="ID_82833641" MODIFIED="1653028125235" TEXT="Der Studiengang soll vollst&#xe4;ndig in Englischer und wahlweise Deutscher Sprache angeboten werden."/>
+</node>
+<node CREATED="1653028281848" FOLDED="true" ID="ID_756654255" MODIFIED="1653052708727" POSITION="right" TEXT="Nachfrage nach Absolventen/-innen&#xa;Besch&#xe4;ftigungschancen, Bedarf an Absolventen, Bedarf an Inhalten, Trends?">
+<node CREATED="1653028353684" FOLDED="true" ID="ID_1542275726" LINK="https://www.bmvi.de/SharedDocs/DE/Artikel/DG/games-strategie.html" MODIFIED="1653051449235" TEXT="siehe Games-Strategie">
+<node CREATED="1653031455281" ID="ID_102652381" MODIFIED="1653031497579" TEXT="Neben dem Transfer von Technologien ist auch der Transfer von Arbeitskr&#xe4;ften bedeutsam, und dies nicht nur innerhalb des Digitalsektors. Die Games-Branche verf&#xfc;gt &#xfc;ber Mitarbeitende mit einer stark ausgepr&#xe4;gten Digitalkompetenz, die es gewohnt sind, mit gro&#xdf;er Kreativit&#xe4;t neue Wege zu beschreiten und unkonventionelle L&#xf6;sungen zu entwickeln. Die Games-Branche ist somit ein Pool f&#xfc;r hochqualifiziertes Personal mit &#x201e;digital skills&#x201c;, die auch f&#xfc;r den Standort Deutschland und die internationale Wettbewerbsf&#xe4;higkeit immer wichtiger werden"/>
+</node>
+<node CREATED="1653028348733" ID="ID_870819242" LINK="https://www.digital-design-manifest.de/" MODIFIED="1653032209618" TEXT="siehe Digital Designer"/>
+<node CREATED="1653050879077" ID="ID_572025408" MODIFIED="1653050986493" TEXT="Innovation durch Simulationen"/>
+<node CREATED="1653050765524" ID="ID_1071782829" MODIFIED="1653050793568" TEXT="Virtualisierte Kommunikation, virtualisiertes Design"/>
+<node CREATED="1653052227731" ID="ID_659910903" MODIFIED="1653052242557" TEXT="Engine driven production pipelines">
+<node CREATED="1653052427129" ID="ID_207263006" MODIFIED="1653052454860" TEXT="Prozedurale Generierung"/>
+<node CREATED="1653052027904" ID="ID_1150602695" MODIFIED="1653052419803" TEXT="Machinima"/>
+<node CREATED="1653051412256" ID="ID_1224408052" MODIFIED="1653051423829" TEXT="Virtual Film-Production"/>
+</node>
+<node CREATED="1653050829061" ID="ID_1306109320" MODIFIED="1653052278067" TEXT="Einsatz von Game-Engines in Unterhaltung und Industrie"/>
+<node CREATED="1653051635820" ID="ID_325227248" MODIFIED="1653051650112" TEXT="Entrepreneurship, Indi-Studios"/>
+</node>
+<node CREATED="1653028378755" FOLDED="true" ID="ID_803327009" MODIFIED="1653052713113" POSITION="right" TEXT="Absch&#xe4;tzung der erforderlichen Ressourcen (Personal, Finanzen, R&#xe4;ume) &#xa;Aufwand zur Realisierung, vorhandenen Ressourcen, zus&#xe4;tzliche Ressourcen?">
+<node CREATED="1653029727707" ID="ID_985049555" MODIFIED="1653052144066" TEXT="Teilweise vorhanden, durch bestehende Infrastrukturen teilweise bereits abbildbar"/>
+<node CREATED="1653051770124" ID="ID_18655034" MODIFIED="1653051803015" TEXT="Mehrere fachbezogene Professuren"/>
+<node CREATED="1653051839189" ID="ID_1025459293" MODIFIED="1653052620017" TEXT="MSL, haptischer VR-Raum, Spiellabor, Smartspace-Lab, Makerspace, Gameshub, Computerpools"/>
+<node CREATED="1653052480943" ID="ID_1761100851" MODIFIED="1653052613398" TEXT="Cloud-Computing insbesondere f&#xfc;r KI und Rendering"/>
+<node CREATED="1653051888231" ID="ID_1036472783" MODIFIED="1653052679713" TEXT="Personal f&#xfc;r Betreuung"/>
+</node>
+<node CREATED="1653028536679" FOLDED="true" ID="ID_1055163278" MODIFIED="1653054141572" POSITION="right" TEXT="Passung zum Profil der HFU&#xa;Attraktivit&#xe4;tssteigernd f&#xfc;r Fakult&#xe4;t/HFU, innovativ, Alleinstellungsmerkmal, &#xa;passend zu Portfolio/Strategie der HFU?">
+<node CREATED="1653031380145" FOLDED="true" ID="ID_11822469" MODIFIED="1653052900596" TEXT="Games sind Innovatoren (aus Games-Strategie des Bundes)">
+<node CREATED="1653031355512" ID="ID_92422592" MODIFIED="1653031379189" TEXT="Die Computerspielebranche steht wie kaum ein anderer Wirtschaftsbereich f&#xfc;r innovative L&#xf6;sungen. Die GamesBranche ist &#x201e;digitaler Frontrunner&#x201c;. Technologien wie Simulation, dreidimensionale Darstellung, Virtuelle Realit&#xe4;t und Augmented Reality wurden von der Games-Branche so weiterentwickelt, dass sie f&#xfc;r neue Bereiche nutzbar wurden. Als Zukunftstechnologien finden sie Eingang in andere Branchen, wodurch Computerspiele sich als Innovator f&#xfc;r den Technologietransfer etabliert haben."/>
+</node>
+<node CREATED="1653052908872" ID="ID_1338334067" MODIFIED="1653052941533" TEXT="Bestehende Ressourcen f&#xf6;derieren."/>
+<node CREATED="1653053091376" ID="ID_620293133" MODIFIED="1653053105874" TEXT="Bilingual passt in die Strategie"/>
+<node CREATED="1653053590422" ID="ID_943142098" MODIFIED="1653053610474" TEXT="Breite Kompetenzentwicklung innerhalb der Themen"/>
+</node>
+<node CREATED="1653028666664" FOLDED="true" ID="ID_63731824" MODIFIED="1653054139502" POSITION="right" TEXT="Konkurrenzsituation extern&#xa;&#xc4;hnliche/benachbarte SG an anderen Hochschulen, Formen der Zusammenarbeit?">
+<node CREATED="1653028132064" ID="ID_1423927030" MODIFIED="1653028163592" TEXT="Nur 9 explizite Games-Angebote staatlicher Hochschulen im Bachelorbereich"/>
+<node CREATED="1653028165563" ID="ID_1993790721" MODIFIED="1653028187332" TEXT="Dagegen 12 an privaten Hochschulen, entsprechend teuer"/>
+<node CREATED="1653031949569" ID="ID_1113933953" MODIFIED="1653031994755" TEXT="Hochschulen und Forschungseinrichtungen gilt es, mit der Games-Branche geeignet zu verkn&#xfc;pfen. Sie dienen als Generatoren f&#xfc;r hochqualifiziertes Personal und k&#xf6;nnen bei Forschung und Entwicklung wichtige Beitr&#xe4;ge leisten. Eine gezielte finanzielle Forschungsf&#xf6;rderung und die Bildung von thematischen Clustern sind m&#xf6;gliche Wege zur St&#xe4;rkung von Innovation. (Games-Strategie des Bundes)"/>
+<node CREATED="1653053338425" ID="ID_1010908565" MODIFIED="1653053345710" TEXT="Auslandspartnerschaften"/>
+<node CREATED="1653053670342" ID="ID_1016183151" MODIFIED="1653053760241" TEXT="Zusammenarbeit mit Trossingen, neue Achse der Zusammenarbeit konkret zu den Themen Musikdesign und KI"/>
+</node>
+<node CREATED="1653028703947" FOLDED="true" ID="ID_521915554" MODIFIED="1653054140614" POSITION="right" TEXT="Konkurrenzsituation (HFU-intern)&#xa;&#xc4;hnliche/benachbarte SG an der HFU, Konkurrenzsituation, Synergien, Spezialisierungen, Abgrenzungen, Vertiefungen?">
+<node CREATED="1653029362256" ID="ID_1297433997" MODIFIED="1653029375922" TEXT="Medieninformatik">
+<node CREATED="1653029375922" ID="ID_1032427457" MODIFIED="1653029580114" TEXT="39 explizite Angebote staatlicher Hochschulen im Bachelorbereich"/>
+<node CREATED="1653029699869" ID="ID_802259451" MODIFIED="1653029703994" TEXT="nur 3 private"/>
+</node>
+<node CREATED="1653029662647" ID="ID_1151985554" MODIFIED="1653029673201" TEXT="Weiterf&#xfc;hrend MIM/DIM"/>
+<node CREATED="1653035938634" ID="ID_534706125" MODIFIED="1653035987889" TEXT="Games sind extrem vielf&#xe4;ltig, daher bieten sich viele Spezialisierungsm&#xf6;glichkeiten in Zusammenarbeit mit anderen Fakult&#xe4;ten und Studieng&#xe4;ngen an"/>
+<node CREATED="1653052988432" ID="ID_1766480936" MODIFIED="1653053253998" TEXT="Ankn&#xfc;pfungspunkte zu anderen Fakult&#xe4;ten">
+<node CREATED="1653053023884" ID="ID_989602455" MODIFIED="1653053040323" TEXT="Neue Medienformen f&#xfc;r die Lehre"/>
+</node>
+<node CREATED="1653053162819" ID="ID_335769731" MODIFIED="1653053187605" TEXT="Immersive Medien im Medizinbereich wichtig">
+<node CREATED="1653053191722" ID="ID_971234687" MODIFIED="1653053204685" TEXT="ebenso Sicherheit und Gesundheit"/>
+</node>
+</node>
+<node CREATED="1653028935699" FOLDED="true" ID="ID_1051762826" MODIFIED="1653054166705" POSITION="left" TEXT="Abschlussbezeichnung">
+<node CREATED="1653028952913" ID="ID_1551651061" MODIFIED="1653054050860" TEXT="BSc"/>
+</node>
+<node CREATED="1653028935700" FOLDED="true" ID="ID_1055897827" MODIFIED="1653054167722" POSITION="left" TEXT="Vorgesehene Zulassungszahl (pro Jahr)">
+<node CREATED="1653054064923" ID="ID_1152356047" MODIFIED="1653054070461" TEXT="34"/>
+</node>
+<node CREATED="1653028935700" FOLDED="true" ID="ID_785799955" MODIFIED="1653054168652" POSITION="left" TEXT="Zulassung">
+<node CREATED="1653028996027" ID="ID_969805839" MODIFIED="1653053883588" TEXT="j&#xe4;hrlich"/>
+</node>
+<node CREATED="1653028935701" FOLDED="true" ID="ID_284902730" MODIFIED="1653054169382" POSITION="left" TEXT="Anbietende Fakult&#xe4;t">
+<node CREATED="1653028944267" ID="ID_899983952" MODIFIED="1653028948926" TEXT="Digitale Medien"/>
+</node>
+<node CREATED="1653028976223" ID="ID_234399435" MODIFIED="1653028978371" POSITION="left" TEXT="Autoren"/>
+</node>
+</map>
